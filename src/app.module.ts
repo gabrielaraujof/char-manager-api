093f7d50284c93f1591bcc8d5810bab193c969f4
Gabriel Araujo <gabriel.araujo@sympla.com.br>
2020-06-16 20:15:41 -0300

    add: mongodb setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+dotenv.config();
+
+const TYPE_ORM_OPTIONS: TypeOrmModuleOptions = {
+  type: 'mongodb',
+  url: process.env.DB_URI,
+  synchronize: true,
+};
+
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(TYPE_ORM_OPTIONS)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const options = new DocumentBuilder()
     .setTitle('Character Manager')
     .setDescription('API Description')
