324595e3db514b84c2b8e3c9aa2d23aec982cc54
Gabriel Araujo <gabriel.araujo@sympla.com.br>
2020-06-16 18:46:32 -0300

    chore: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const options = new DocumentBuilder()
+    .setTitle('Character Manager')
+    .setDescription('API Description')
+    .setVersion('0.1')
+    .addTag('character')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
