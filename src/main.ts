4fc0dd15483a0f183c32d610576257746fa95679
Gabriel Araujo <gabriel.araujo@sympla.com.br>
2020-06-16 21:50:47 -0300

    chore: enable heroku deploy

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/', app, document);
-
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
