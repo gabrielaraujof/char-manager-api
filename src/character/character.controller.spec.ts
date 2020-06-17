b78622d341cbb077c800c022938ca6dbafcf0295
Gabriel Araujo <gabriel.araujo@sympla.com.br>
2020-06-17 11:02:24 -0300

    fix: unit tests

--- src/character/character.controller.spec.ts
+++ src/character/character.controller.spec.ts
@@ -1,11 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { CharacterController } from './character.controller';
+import { CharacterService } from './character.service';
+
 
 describe('Character Controller', () => {
   let controller: CharacterController;
+  const charactersService = { findAll: () => [] };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [{ provide: CharacterService, useValue: charactersService }],
       controllers: [CharacterController],
     }).compile();
 
--- src/character/character.service.spec.ts
+++ src/character/character.service.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CharacterService } from './character.service';
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
+import { Character } from './character.entity';
 
 describe('CharacterService', () => {
   let service: CharacterService;
+  const charactersRepo = { find: () => [] };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forFeature([Character])],
       providers: [CharacterService],
-    }).compile();
+    })
+      .overrideProvider(getRepositoryToken(Character))
+      .useValue(charactersRepo)
+      .compile();
 
     service = module.get<CharacterService>(CharacterService);
   });
