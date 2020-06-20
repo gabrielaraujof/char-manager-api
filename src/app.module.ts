422d3d9fdaf1801236a23e42135d33d7830eb042
Gabriel Araujo <gabriel.araujo@sympla.com.br>
2020-06-19 21:48:33 -0300

    chore: isolate TypeOrm config on its own js module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { TypeOrmDefaultOptions } from './config/type-orm.config';
 import { CharacterModule } from './character/character.module';
-import { Character } from './character/character.entity';
-
-dotenv.config();
-
-const TYPE_ORM_OPTIONS: TypeOrmModuleOptions = {
-  type: 'mongodb',
-  url: process.env.DB_URI,
-  w: 'majority',
-  entities: [Character],
-  synchronize: true,
-};
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TYPE_ORM_OPTIONS), CharacterModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmDefaultOptions), CharacterModule],
 })
 export class AppModule {}
